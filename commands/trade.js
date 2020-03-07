68ccb31c78cd2b8ec0a5a0bb46952cfc19969b32
da-the-dev <leshiktk@me.com>
2020-03-07 11:31:07 +0300

    Minor fixes

--- commands/trade.js
+++ commands/trade.js
@@ -1,6 +1,6 @@
 module.exports = {
     "name": "trade",
-    "description": "Позволяет трейдиться с другими.",
+    "description": "Позволяет трейдиться с другими. (в разработке)",
     exec(input) {
         const SteamAPI = require('steamapi')
         const steam = new SteamAPI(process.env.SQL_STEAMAPI_TOKEN)
