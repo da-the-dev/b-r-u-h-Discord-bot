8337b997f3aea35d64f12793a5a13cf3aaffaabd
da-the-dev <leshiktk@me.com>
2020-03-11 23:55:49 +0300

    Minor fixes

--- commands/sign.js
+++ commands/sign.js
@@ -6,23 +6,20 @@ module.exports = {
     'name': 'sign',
     async exec(input) {
         msg = input[0]
-        const keyv = require('keyv')
 
-        var db = new keyv(`mysql://${process.env.SQL_USER}:${process.env.SQL_SECRET}@${process.env.SQL_HOST}:3306/${process.env.SQL_DATABASE}`)
-
-        db.on('error', err => {
+        global.db.on('error', err => {
             console.log('database: Connection error:', err)
             return 0
         })
 
-        var guild = await db.get(msg.guild.id)
+        var guild = await global.db.get(msg.guild.id)
         if (!guild.owner) {
             guild.owner = msg.author.id
         } else {
             msg.author.send(`Хозяин бота на сервере \`${msg.guild.name}\` уже назначен!`)
             return 0
         }
-        await db.set(msg.guild.id, guild)
+        await global.db.set(msg.guild.id, guild)
 
         msg.author.send(`Вы назначены хозяином бота на сервере \`${msg.guild.name}!\``)
     }
