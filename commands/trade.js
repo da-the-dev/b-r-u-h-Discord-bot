ac3eb4e7ad7888196fa8382925e17fd8ca8336a8
da-the-dev <leshiktk@me.com>
2020-03-11 23:51:34 +0300

    Deleted 'trade'

--- commands/trade.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    "name": "trade",
-    "description": "Позволяет трейдиться с другими. (в разработке)",
-    exec(input) {
-        const SteamAPI = require('steamapi')
-        const steam = new SteamAPI(process.env.SQL_STEAMAPI_TOKEN)
-        msg = input[0]
-    }
-}
