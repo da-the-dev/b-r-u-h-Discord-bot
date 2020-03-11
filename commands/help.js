1875118a9fef01c71649f6d43a6ea103cd994a71
da-the-dev <leshiktk@me.com>
2020-03-11 18:30:57 +0300

    Added a command sorting system

--- commands/help.js
+++ commands/help.js
@@ -2,14 +2,26 @@ const Discord = require('discord.js')
 module.exports = {
     "name": "help",
     "description": "Присылает в личные сообщения информацию о всех командах.",
-    exec(msg, args, client) {
+    exec(input) {
+        // Custom sort func just to get more control
+        var sortFunc = (a, b) => {
+            if (a.name > b.name) return 1
+            if (a.name < b.name) return -1
+            return 0
+        }
+
+        msg = input[0]
+        client = input[2]
+
         embed = new Discord.RichEmbed()
             .setTitle("Список команд")
             .setColor("#ff4040")
-        client.commands.forEach(c => {
-            if (c.name != 'cleardb' || c.name != 'createdb')
-                embed.addField(`**!${c.name}**`, c.description)
-        })
+
+        // Sorting commands array and adding name and description to the field in embed
+        client.commands.sort(sortFunc).forEach(c => {
+            embed.addField(`**!${c.name}**`, c.description)
+        });
+
         msg.author.send(embed)
     }
 }
