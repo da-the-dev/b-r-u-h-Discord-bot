9785c4d30481afbc73cf93bb62afae428564c748
da-the-dev <leshiktk@me.com>
2020-03-18 22:23:19 +0300

    Updated to cooperate wirh 'setTrade'

--- commands/trade.js
+++ commands/trade.js
@@ -1,9 +1,10 @@
+const DB = require('../dbController.js')
 module.exports = {
     "name": "trade",
     "description": "Для начала трейда, упомините партнера так: `!trade @partner`, где `@partner` ⏤ пользователь, с кем тогруетесь. Важно чтобы тот написал свою ссылку на трейд в трейд-канале.",
     async exec(msg, args, client) {
         global.msg = msg
-        msg.guild.channels.get('657862996403683328').fetchMessages()
+        msg.guild.channels.get(DB.getGuildField(msg, 'tradeChannel')).fetchMessages()
             .then(async messages => {
                 const open = require('open')
 
