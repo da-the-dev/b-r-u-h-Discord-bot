eff039688e40fcd6d218d9a62faced76b1fa2c67
da-the-dev <leshiktk@me.com>
2020-03-26 20:33:32 +0300

    Updated. Can set mkgrp category

--- commands/settings.js
+++ commands/settings.js
@@ -1,12 +1,13 @@
 const DB = require('../dbController.js')
 const emojies = require('emojies')
 const Discord = require('discord.js')
+const hex_colors = require('hex_colors')
 module.exports = {
     "name": "settings",
     "description": "",
     /**
      * 
-     * @param {Discord.Message} msg 
+     * @param {Discord.Message} msg Main message
      * @param {object} args 
      * @param {Discord.Client} client 
      */
@@ -17,25 +18,41 @@ module.exports = {
         global.msg = msg
         var embed = new Discord.RichEmbed() //Creating an embed message with the settings menu
             .setTitle('Настройки')
-            .addField('Установить канал для трейда', "лялял")
+            .addField('Установить канал для трейда', ".")
+            .addField()
+            .setColor(hex_colors.Red)
 
         global.msg.reply(embed) //Replying with the settings menu
             .then(async message => {
                 await message.react(emojies.a) //Reacting with all of the options from the settings menu
-                await msg.react(emojies.b)
+                await message.react(emojies.b)
 
                 message.awaitReactions((reaction, user) => user.id == global.msg.author.id, { maxEmojis: 1 }) //Collecting the reactioN from the owner
                     .then(reactions => {
-                        if (reactions.first().emoji.name == emojies.a) { // If A
-                            msg.reply('отправьте мне id канала для трейда.') //Responding with request to send the id of the trade channel
-                                .then(setting => {
-                                    setting.channel.awaitMessages(m => m.author.id == msg.author.id, { max: 1 }) //Collecting messagE in the channel
-                                        .then(async messages => {
-                                            let msgg = messages.first() //Setting the 'tradeChannel' to the id in the owner's responce
-                                            await DB.addToGuild(global.msg, 'tradeChannel', msgg.content)
-                                            messages.first().reply(`успешно установил канал для трейда на \`${msgg.guild.channels.get(msgg.content).name}\`!`)
-                                        })
-                                })
+                        switch (reactions.first().emoji.name) {
+                            case emojies.a:
+                                msg.reply('отправьте мне id канала для трейда.') //Responding with request to send the id of the trade channel
+                                    .then(setting => {
+                                        setting.channel.awaitMessages(m => m.author.id == msg.author.id, { max: 1 }) //Collecting messagE in the channel
+                                            .then(async messages => {
+                                                let msgg = messages.first() //Setting the 'tradeChannel' to the id in the owner's responce
+                                                await DB.addToGuild(global.msg, 'tradeChannel', msgg.content)
+                                                messages.first().reply(`успешно установил канал для трейда на \`${msgg.guild.channels.get(msgg.content).name}\`!`)
+                                            })
+                                    })
+                                break;
+                            case emojies.b:
+                                msg.reply('отправьте мне id категории для групп.') //Responding with request to send the id of the trade channel
+                                    .then(setting => {
+                                        setting.channel.awaitMessages(m => m.author.id == msg.author.id, { max: 1 }) //Collecting messagE in the channel
+                                            .then(async messages => {
+                                                let msgg = messages.first() //Setting the 'tradeChannel' to the id in the owner's responce
+                                                await DB.addToGuild(global.msg, 'groupCategory', msgg.content)
+                                                messages.first().reply(`успешно установил категорию для групп на \`${msgg.guild.channels.get(msgg.content).name}\`!`)
+                                            })
+                                    })
+                            default:
+                                break;
                         }
                     })
             })
