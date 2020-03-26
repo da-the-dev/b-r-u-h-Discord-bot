fdf946fe609ac25d23691a76e3b595c9c392e503
da-the-dev <leshiktk@me.com>
2020-03-26 10:17:48 +0300

    Fixed some issues

--- commands/sign.js
+++ commands/sign.js
@@ -3,11 +3,11 @@ module.exports = {
     'name': 'sign',
     async exec(msg, args, client) {
         var guild = DB.getGuild(msg)
-        if (!guild.owner) {
-            guild.owner = msg.author.id
-        } else {
+        if (guild.owner) {
             msg.author.send(`Хозяин бота на сервере \`${msg.guild.name}\` уже назначен!`)
             return 0
+        } else {
+            guild.owner = msg.author.id
         }
         await DB.addToGuild(msg, 'owner', msg.author.id)
 
