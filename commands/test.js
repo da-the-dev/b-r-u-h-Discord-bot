77b29813cfefabcadc365368a76e88cb247d8f8c
da-the-dev <leshiktk@me.com>
2020-03-27 20:44:01 +0300

    Removed swearing

--- commands/test.js
+++ commands/test.js
@@ -2,6 +2,6 @@ module.exports = {
     "name": "test",
     "description": "Позволяет понять, работает ли сейчас бот.",
     exec(msg, args, client) {
-        msg.reply('бл*ть, что тебе нужно?')
+        msg.reply('я здесь!')
     }
 }
