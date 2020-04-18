7ac82f00368c3f8e95ac5bfce47b8ca7f863832c
da-the-dev <leshiktk@me.com>
2020-04-18 20:48:39 +0300

    FIxed bugs with this poor command

--- commands/sign.js
+++ commands/sign.js
@@ -1,18 +1,21 @@
 const DB = require('../dbController.js')
+const Discord = require('discord.js')
 module.exports = {
     'name': 'sign',
     'description': '.',
     'onlyOwner': false,
+
+    /**
+    * @param {Discord.Message} msg 
+    */
     async exec(msg, args, client) {
         let owner = await DB.getGuildField(msg, 'owner')
-        console.log(owner)
 
         if (owner) {
             msg.author.send(`Хозяин бота на сервере \`${msg.guild.name}\` уже назначен!`)
             return 0
-        } else {
-            await DB.addToGuild(msg, 'owner', msg.author.id)
-            msg.author.send(`Вы назначены хозяином бота на сервере \`${msg.guild.name}!\``)
         }
+        await DB.addToGuild(msg, 'owner', msg.author.id)
+        msg.author.send(`Вы назначены хоязином бота на сервере \`${msg.guild.name}\`!`)
     }
 }
