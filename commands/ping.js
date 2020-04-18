bce0846019ccac04e48710a7b4b58ca1cb9748e0
da-the-dev <leshiktk@me.com>
2020-04-18 22:08:40 +0300

    Hard reset

--- commands/ping.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-    "name": "ping",
-    "description": "Показывает пинг бота",
-    "onlyOwner": true,
-    exec(msg, args, client) {
-        msg.reply(`мой пинг: ${client.ping.toFixed(0)}.`)
-        console.log(`мой пинг: ${client.ping.toFixed(0)}.`)
-    }
-}
--- commands/test.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    "name": "test",
-    "description": "Позволяет понять, работает ли сейчас бот.",
-    'onlyOwner': true,
-    exec(msg, args, client) {
-        msg.reply('я здесь!')
-    }
-}
